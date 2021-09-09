274b0902c269633602012c7480b26eea7ea17007
b-chai <b.chai82@gmail.com>
2021-09-09 13:03:47 -0400

    add jbuilder for channel

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -36,6 +36,6 @@ class Api::ChannelsController < ApplicationController
 
     private
     def channel_params
-        params.require(:channel).permit(:channel_name)
+        params.require(:channel).permit(:channel_name,:server_id)
     end
 end
